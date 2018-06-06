ea91b8d7b576f6c239ab12ceb4b954cd95317d95
kellerjustin <31552409+kellerjustin@users.noreply.github.com>
2018-06-06 14:32:33 -0500

    libgtk-3-dev added

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt install git python3-pip python3-wheel python3-setuptools python3-dev build-essential dpkg-dev libgtk-3-0-dbg libwebkit-dev freeglut3-dev libgstreamer-plugins-base1.0-dev
+sudo apt install git python3-pip python3-wheel python3-setuptools python3-dev build-essential dpkg-dev libgtk-3-dev libwebkit-dev freeglut3-dev libgstreamer-plugins-base1.0-dev

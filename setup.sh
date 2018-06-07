3eec4bed8958127c6969593f6492018d03b3acca
kellerjustin <kellerjustin@gmail.com>
2018-06-07 11:29:39 -0500

    dns fix on local domains

--- setup.sh
+++ setup.sh
@@ -4,8 +4,15 @@ echo "Will need to run this with sudo."
 if [ -e /etc/udev/rules.d/95-usb-perms.rules ]; then
   echo "File already exists!"
 else
-  echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"0c27\", ATTR{idProduct}==\"3bfa\", GROUP=\"plugdev\", MODE=\"0666\"" >> /etc/udev/rules.d/95-usb-perms.rules
+  sudo echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"0c27\", ATTR{idProduct}==\"3bfa\", GROUP=\"plugdev\", MODE=\"0666\"" >> /etc/udev/rules.d/95-usb-perms.rules
   echo "File written."
 fi
 echo "Now installing dependencies..."
 sudo apt install git python3-pip python3-wheel python3-setuptools python3-dev build-essential dpkg-dev libgtk-3-dev libwebkit-dev freeglut3-dev libgstreamer-plugins-base1.0-dev
+sudo -H pip3 install wxpython
+echo "Fixing network config for local domains on Debian Systems..."
+echo "Backing up original..."
+sudo cp /etc/nsswitch.conf /etc/nsswitch.conf.original
+echo "Replacing config line..."
+sudo sed -i '/hosts/c\hosts:          files mdns4_minimal dns mdns4 [NOTFOUND=return]' /etc/nsswitch.conf
+echo "Done. Restore /etc/nsswitch.conf.original if this causes issues."

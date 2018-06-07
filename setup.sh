6a678a913257d985dc7877103ca39c8141d2d5f4
kellerjustin <kellerjustin@gmail.com>
2018-06-07 09:42:10 -0500

    added usb udev rule to bash file

--- setup.sh
+++ setup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+echo "Creating udev rule to allow pyusb to communicate with card reader."
+echo "Will need to run this with sudo."
+if [ -e /etc/udev/rules.d/95-usb-perms.rules ]; then
+  echo "File already exists!"
+else
+  echo "SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"0c27\", ATTR{idProduct}==\"3bfa\", GROUP=\"plugdev\", MODE=\"0666\"" >> /etc/udev/rules.d/95-usb-perms.rules
+  echo "File written."
+fi
+echo "Now installing dependencies..."
 sudo apt install git python3-pip python3-wheel python3-setuptools python3-dev build-essential dpkg-dev libgtk-3-dev libwebkit-dev freeglut3-dev libgstreamer-plugins-base1.0-dev
